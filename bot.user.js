e2af444f9a39a109a8db464a0107fb5707966574
sripathinikhil9-max <sripathinikhil9@gmail.com>
2026-07-27 16:48:04 +0530

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,1 +1,19 @@
+// ==UserScript==
+// @name         Revenue Audit Bot
+// @namespace    https://github.com/sripathinikhil9-max/RevenueAuditBot
+// @version      1.0.0
+// @description  Revenue Audit Automation for AppSheet
+// @author       Nikhil + ChatGPT
+// @match        https://www.appsheet.com/*
+// @grant        none
+// ==/UserScript==
 
+(function () {
+    'use strict';
+
+    console.log("====================================");
+    console.log("Revenue Audit Bot v1.0");
+    console.log("Project Started");
+    console.log("====================================");
+
+})();
