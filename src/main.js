2ebc70e7b042373f5f855256a0ff0d675d8d1eb5
sripathinikhil9-max <sripathinikhil9@gmail.com>
2026-07-27 21:58:32 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
+class RevenueAuditBot{
 
+    constructor(){
+
+        this.logger = null;
+
+        this.search = null;
+
+        this.report = null;
+
+        this.remarks = null;
+
+        this.workflow = null;
+
+    }
+
+    initialize(logElement){
+
+        this.logger = new Logger(logElement);
+
+        this.search = new SearchEngine(this.logger);
+
+        this.report = new ReportEngine(this.logger);
+
+        this.remarks = new RemarksEngine(this.logger);
+
+        this.workflow = new Workflow(this);
+
+        this.logger.success("Revenue Audit Bot Initialized");
+
+    }
+
+}
