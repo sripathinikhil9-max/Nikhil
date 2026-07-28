9597886d9b6279ded6a45eacd9111e91056e5e81
sripathinikhil9-max <sripathinikhil9@gmail.com>
2026-07-28 19:16:55 +0530

    Update appsheet.js

--- src/appsheet.js
+++ src/appsheet.js
@@ -5,6 +5,30 @@
 
 class AppSheet {
 
+    // ----------------------------
+// REMARKS
+// ----------------------------
+
+static deleteButton() {
+    return document.querySelector('#navbar-Offshore\\ Remarks-Delete');
+}
+
+static addButton() {
+
+    const section = this.remarksSection();
+
+    if (!section) return null;
+
+    return section.querySelector("span");
+
+}
+
+static hasExistingRemarks() {
+
+    return this.deleteButton() !== null;
+
+}
+
     // ---------- WAIT HELPERS ----------
 
 static async waitForReport(timeout = 10000) {
